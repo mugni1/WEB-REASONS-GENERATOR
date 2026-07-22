cead8765a16fcf3ba9b6a5f26169dcf0cd1cd7a3
Asep Abdul Mugni <asepabdulmugni22@gmail.com>
2026-07-22 21:58:48 +0700

    feat: enhance hero section layout with styled header and descriptive subtitle

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,13 +8,21 @@ export const Route = createFileRoute('/')({
 function RouteComponent() {
 	return (
 		<section className="container max-w-2xl mx-auto py-10 p-4">
-			<div className="flex items-center justify-between">
+			<div className="flex items-center justify-between mb-4">
 				<Button variant={'outline'} size={'sm'}>
 					14.000 Di Buat
 				</Button>
 				<Button variant={'outline'}>Bantuan</Button>
 			</div>
-			<h1 className="text-4xl font-bold mb-4 text-center">Ngeles</h1>
+			<div className="mb-2 flex items-center gap-3 justify-center">
+				<div className="h-px w-6 bg-linear-to-r from-transparent to-primary/30"></div>
+				<span className="text-[10px] font-black tracking-[0.3em] text-primary uppercase">Generator Alasan Terbaik</span>{' '}
+				<div className="h-px w-6 bg-linear-to-l from-transparent to-primary/30"></div>
+			</div>
+			<div className="flex flex-col items-center mb-4 gap-4">
+				<h1 className="text-6xl font-bold text-center text-foreground">Ngeles</h1>
+				<p className="max-w-md text-sm font-medium leading-relaxed text-slate-500 dark:text-slate-400 sm:text-base">Buat alasan sempurna dan meyakinkan dalam sekejap.</p>
+			</div>
 			<Button variant={'outline'}>Click me</Button>
 		</section>
 	)
